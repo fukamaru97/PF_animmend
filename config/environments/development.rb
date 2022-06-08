be04cd75881a2790f26fce03c7362de2fc0f4e1c
taichibonbarx <taichibonbarx@gmail.com>
2022-06-08 15:10:03 +0000

    hostの追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "fc744f8c7409432fa29e2ee7f5841edb.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
