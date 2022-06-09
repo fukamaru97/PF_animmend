58d510755840c61e601e179be25e050cc3812db0
taichibonbarx <taichibonbarx@gmail.com>
2022-06-09 06:38:54 +0000

    作品登録機能

--- app/controllers/admins/works_controller.rb
+++ app/controllers/admins/works_controller.rb
@@ -1,13 +1,27 @@
 class Admins::WorksController < ApplicationController
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    @work.save
+    redirect_to admins_works_path
+  end
+
   def index
   end
 
   def show
   end
 
-  def new
-  end
-
   def edit
   end
+
+  private
+
+  def work_params
+    params.require(:work).permit(:name, :image, :story)
+  end
+
 end
