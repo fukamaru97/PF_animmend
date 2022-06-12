eb8d854d5346b6953f236150c0d86811168e845d
taichibonbarx <taichibonbarx@gmail.com>
2022-06-12 14:21:50 +0000

    ユーザー情報編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     if resource.is_a?(Admin)
        admins_works_path
     else
-       users_user_path(@user.id)
+       users_user_path(current_user.id)
     end
   end
 
--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -5,5 +5,20 @@ class Users::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to users_user_path(current_user.id)
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :profile_image)
+  end
+
+
 end
