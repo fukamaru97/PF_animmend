d661e922cfc93ce2b0b1288a9c7ce257a4800e5f
taichibonbarx <taichibonbarx@gmail.com>
2022-06-14 14:55:06 +0000

    一時中断

--- app/controllers/users/works_controller.rb
+++ app/controllers/users/works_controller.rb
@@ -1,11 +1,12 @@
 class Users::WorksController < ApplicationController
 
-  def index
+def index
   @works = params[:tag_id].present? ? Tag.find(params[:tag_id]).works : Work.all
     #タグの指定があれば該当の作品を表示、指定がないときは全ての作品を表示
-  end
+end
 
   def show
     @work = Work.find(params[:id])
+    @work_tags = @work.tags
   end
 end
